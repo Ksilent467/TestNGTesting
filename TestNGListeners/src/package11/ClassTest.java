4a60e045fc5bf4fad298ce07ac912bd6f1599ee2
Piyush <piyush1997.com@gmail.com>
2022-02-09 20:35:22 +0530

    Repo Updated with TestNG files

---
+++ TestNGListeners/src/package11/ClassTest.java
@@ -0,0 +1,35 @@
+package package11;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Listeners;
+import org.testng.annotations.Test;
+
+import graphql.Assert;
+
+@Listeners(ClassListener.class)
+public class ClassTest {
+	@Test
+	public  void GmailPage() throws Exception
+	{		
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\user\\Downloads\\Selenium\\chromedriver.exe");
+		WebDriver driver=new ChromeDriver();
+		driver.get("https://mail.google.com/mail/");
+		driver.quit();
+	}
+
+@Test
+	public void TestFailCheck()
+	{
+		System.out.println("This is to check Test Case Failure");
+		Assert.assertTrue(false);
+	}
+
+@Test
+public void TestPassCheck()
+{
+	System.out.println("This is to check Test Case Passing");
+	Assert.assertTrue(true);
+}
+
+}
